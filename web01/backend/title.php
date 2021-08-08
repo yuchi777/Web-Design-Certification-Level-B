f0501be93e739140b79f2cf16fd2f01bf59a75f9
yuchi777 <yuchi8943@gmail.com>
2021-08-08 12:29:19 +0800

    調整title.php原始碼內容，增加modal資料夾給彈出視窗用

--- web01/backend/title.php
+++ web01/backend/title.php
@@ -5,7 +5,7 @@
 
     
     <!-- 因為刪除iframe所以target在form表單裡刪除 -->
-    <form method="post"  action="?do=tii">
+    <form method="post"  action="../web01/api/edit.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -20,8 +20,19 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
-                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+
+                    <td width="200px">
+
+                        <!-- URL不要空格 -->
+                        <!-- 新增網站標題 -->
+                        <!-- onclick 轉址 modal/title.php  -->
+                        <!-- &#39;為單引號 -->
+                        <input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?=$do?>.php&#39;)" value="<?= $as[$do] ?>">
+                    </td>
+
+                    <td class="cent">
+                        <input type="submit" value="修改確定"><input type="reset" value="重置">
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -29,7 +40,19 @@
     </form>
 </div>
 </div>
-<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+
+
+
+
+
+
+
+
+
+
+
+<!-- 用在front的news頁面 -->
+<!-- <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 <script>
     $(".sswww").hover(
         function() {
@@ -44,4 +67,4 @@
             $("#alt").hide()
         }
     )
-</script>
+</script> -->
